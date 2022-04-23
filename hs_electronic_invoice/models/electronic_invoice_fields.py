5e5a3b68751c6cbdda1452edf660f1bff217fbd7
Sleather Vega <svega@hermecsolutions.com>
2022-04-23 11:55:34 -0500

    arr

--- hs_electronic_invoice/models/electronic_invoice_fields.py
+++ hs_electronic_invoice/models/electronic_invoice_fields.py
@@ -330,7 +330,7 @@ class electronic_invoice_fields(models.Model):
 		)
 
 		if(self.total_precio_descuento > 0):
-			totales_subtotales_inv_dict["descuentoBonificacion"] = descuentoBonificacion_dict
+			totales_subtotales_inv_dict["listaDescBonificacion"] = [descuentoBonificacion_dict]
 
 
 		datos = dict(
