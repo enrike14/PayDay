fbc0cbe8fcb2c07f1d4d8e53ca5b6f323a93c203
Sleather Vega <svega@hermecsolutions.com>
2022-04-23 11:48:58 -0500

    Move

--- hs_electronic_invoice/models/electronic_invoice_fields.py
+++ hs_electronic_invoice/models/electronic_invoice_fields.py
@@ -322,12 +322,14 @@ class electronic_invoice_fields(models.Model):
 			if(float(tuple_retencion[1]) < 0 == True):
 				totales_subtotales_inv_dict["retencion"] = retencion_dict
 
+
+		logging.info("Total =" + str('%.2f' % round(self.total_precio_descuento, 2)))
+		descuentoBonificacion_dict = [{
+			'DescDescuento': "Descuentos aplicados a los productos",
+			'montoDescuento':str('%.2f' % round(self.total_precio_descuento, 2))
+		}]
+		
 		if(self.total_precio_descuento > 0):
-			logging.info("Total =" + str('%.2f' % round(self.total_precio_descuento, 2)))
-			descuentoBonificacion_dict = [{
-				'DescDescuento': "Descuentos aplicados a los productos",
-				'montoDescuento':str('%.2f' % round(self.total_precio_descuento, 2))
-			}]
 			totales_subtotales_inv_dict["listaDescBonificacion"] = descuentoBonificacion_dict
 
 
