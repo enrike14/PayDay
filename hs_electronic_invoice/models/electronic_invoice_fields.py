b84ba84cdb5f8d226d3dc631ef2bae1e5122c78c
Sleather Vega <svega@hermecsolutions.com>
2022-04-23 08:48:20 -0500

    add div 2

--- hs_electronic_invoice/models/electronic_invoice_fields.py
+++ hs_electronic_invoice/models/electronic_invoice_fields.py
@@ -702,7 +702,7 @@ class electronic_invoice_fields(models.Model):
 							# logging.info("Children taxes:" + str(group_tax_children))
 							obj_sub_impuestos = self.get_taxes_in_group(group_tax_children)
 							logging.info("array subimpuestos: " + str(obj_sub_impuestos))
-							monto_porcentaje = obj_sub_impuestos['itbmPercent']
+							monto_porcentaje = obj_sub_impuestos['itbmPercent']/2
 							if int(obj_sub_impuestos['itbmPercent']) == 0:
 								tasaITBMS = "00"
 								# logging.info("Tasa ITBMS 0= "+ str(tasaITBMS))
