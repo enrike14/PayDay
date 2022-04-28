fc6870344f8da20e8a5f1e2a49a5ed257d685ecf
ERILEX <edelgado@hermecsolutions.com>
2022-04-28 17:01:46 -0500

    code commets

--- hs_electronic_invoice/models/electronic_invoice_fields.py
+++ hs_electronic_invoice/models/electronic_invoice_fields.py
@@ -765,8 +765,8 @@ class electronic_invoice_fields(models.Model):
 					new_item_object["TasaISC"]=str(item.product_id.tasaISC)
 					new_item_object["ValorISC"]=str('%.2f' % round(float(item.product_id.valorISC), 2))
 					
-				if item.product_id.default_code:
-					new_item_object["Codigo"]=str(item.product_id.default_code)
+				#if item.product_id.default_code:
+					#new_item_object["Codigo"]=str(item.product_id.default_code)
 					
 				if item.product_id.unidadMedida:
 					new_item_object["UnidadMedida"]=str(item.product_id.unidadMedida)
